3f5fe09c8960927ec01cfb6559778bd81be176b9
freeDaTitty <danieldinu6091@gmail.com>
2022-06-14 12:42:18 +0300

    Added todo.sql database

---
+++ src/main/resources/sample_data/todo.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS  tasks;
+DROP TYPE IF EXISTS TASK_TYPE;
+
+CREATE TYPE TASK_TYPE AS ENUM
+(
+    'WORK',
+    'HOME',
+    'HOBBY'
+);
+
+CREATE TABLE tasks
+(
+    id          SERIAL NOT NULL,
+    type        TASK_TYPE,
+    name        VARCHAR,
+    due_date    TIMESTAMP WITHOUT TIME ZONE,
+    estimate    INT
+);
+
+ALTER TABLE ONLY tasks
+    ADD CONSTRAINT pk_id PRIMARY KEY (id);
