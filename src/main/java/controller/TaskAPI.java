6779885026543e9fd8ef5efe3162a6ffb60aa49b
freeDaTitty <danieldinu6091@gmail.com>
2022-06-15 17:56:19 +0300

    Implemented check and delete task in TaskAPI Servlet

--- src/main/java/controller/TaskAPI.java
+++ src/main/java/controller/TaskAPI.java
@@ -25,4 +25,26 @@ public class TaskAPI extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(json);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer id = Integer.valueOf(request.getPathInfo().substring(1));
+
+        ApplicationService applicationService = ApplicationService.getInstance();
+        Task task = applicationService.taskDao.get(id);
+        task.setCompleted(true);
+
+        applicationService.taskDao.update(task);
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer id = Integer.valueOf(request.getPathInfo().substring(1));
+
+        ApplicationService applicationService = ApplicationService.getInstance();
+        applicationService.taskDao.remove(id);
+
+        doGet(request, response);
+    }
 }
