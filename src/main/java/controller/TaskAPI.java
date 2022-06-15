e26e7ee4e3d25bd2c7dd3fa7bcd35e4c206d909d
freeDaTitty <danieldinu6091@gmail.com>
2022-06-15 16:34:04 +0300

    Added TaskAPI to get one task as json

---
+++ src/main/java/controller/TaskAPI.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.google.gson.Gson;
+import model.Task;
+import service.ApplicationService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/api/task/*"})
+public class TaskAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer id = Integer.valueOf(request.getPathInfo().substring(1));
+
+        ApplicationService applicationService = ApplicationService.getInstance();
+        Task task = applicationService.taskDao.get(id);
+
+        String json = new Gson().toJson(task);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
