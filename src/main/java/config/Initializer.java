3432b331245a45d3e6b2385bd1e623eabcf67be9
freeDaTitty <danieldinu6091@gmail.com>
2022-06-16 13:00:51 +0300

    Added creationDate and daysLeft in Initializer

--- src/main/java/config/Initializer.java
+++ src/main/java/config/Initializer.java
@@ -19,13 +19,33 @@ public class Initializer implements ServletContextListener {
 
         if (tasks.size() != 0) return;
 
-        Task work = new Task(TaskType.WORK, "Renew schedule", Date.valueOf("2022-06-16"), 1, false);
-        Task home = new Task(TaskType.HOME, "Do laundry", Date.valueOf("2022-06-18"), 1, false);
-        Task hobby = new Task(TaskType.HOBBY, "Walk in park", Date.valueOf("2022-06-19"), 1, false);
+        Task work = new Task(TaskType.WORK, "Finish project", Date.valueOf("2022-06-30"), 6, false);
+        Task home = new Task(TaskType.HOME, "Renovate kitchen", Date.valueOf("2022-06-14"), 2, true);
+        Task hobby = new Task(TaskType.HOBBY, "Walk in park", Date.valueOf("2022-06-17"), 1, false);
 
         work.setId(1);
+        work.setColor("purple");
+        work.setCreationDate(Date.valueOf("2022-06-19"));
+
+        int daysLeft = (int)(work.getDueDate().getTime() - new Date(System.currentTimeMillis()).getTime()) / (1000 * 60 * 60 * 24) + 1;
+
+        work.setDaysLeft(daysLeft);
+
         home.setId(2);
+        home.setColor("blue");
+        home.setCreationDate(Date.valueOf("2022-06-10"));
+
+        daysLeft = (int)(home.getDueDate().getTime() - new Date(System.currentTimeMillis()).getTime()) / (1000 * 60 * 60 * 24) + 1;
+
+        home.setDaysLeft(daysLeft);
+
         hobby.setId(3);
+        hobby.setColor("green");
+        hobby.setCreationDate(new Date(System.currentTimeMillis()));
+
+        daysLeft = (int)(hobby.getDueDate().getTime() - new Date(System.currentTimeMillis()).getTime()) / (1000 * 60 * 60 * 24) + 1;
+
+        hobby.setDaysLeft(daysLeft);
 
         applicationService.taskDao.add(work);
         applicationService.taskDao.add(home);
