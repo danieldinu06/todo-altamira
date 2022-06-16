8385e3ae27a7addf9c510bcf882150767287b5de
freeDaTitty <danieldinu6091@gmail.com>
2022-06-16 17:02:29 +0300

    Renamed script.js to buttons.js

---
+++ src/main/webapp/static/js/buttons.js
@@ -0,0 +1,45 @@
+function del(btn) {
+    btn.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        let btnId = btn.value.split("-")[1];
+
+        const request = await fetch("/api/task/" + btnId, {
+            method: "DELETE",
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        });
+
+        let task = btn.parentElement.parentElement;
+
+        task.classList.add("fall");
+        task.addEventListener('transitionend', () => {
+            task.remove();
+        })
+    });
+}
+
+function check(btn) {
+    btn.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        let btnId = btn.value.split("-")[1];
+
+        const response = await fetch('/api/task/' + btnId);
+        let task = await response.json();
+        console.log(task);
+
+        task.completed = true;
+
+        btn.parentElement.parentElement.classList.add("completed");
+
+        const request = await fetch("/api/task/" + btnId, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        });
+    });
+}
