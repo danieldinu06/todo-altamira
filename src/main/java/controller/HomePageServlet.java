940c133f3a646c27b35b48567bf29437773c16ba
freeDaTitty <danieldinu6091@gmail.com>
2022-06-16 13:05:31 +0300

    Finished HomePage Servlet

--- src/main/java/controller/HomePageServlet.java
+++ src/main/java/controller/HomePageServlet.java
@@ -2,18 +2,22 @@ package controller;
 
 import config.TemplateEngineUtil;
 import model.Task;
+import model.util.TaskType;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import service.ApplicationService;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
+import java.util.Arrays;
 import java.util.List;
 
-@WebServlet(urlPatterns = {"/"})
+@WebServlet(urlPatterns = {""})
 public class HomePageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -25,5 +29,10 @@ public class HomePageServlet extends HttpServlet {
 
         webContext.setVariable("tasks", tasks);
         templateEngine.process("index.html", webContext, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
